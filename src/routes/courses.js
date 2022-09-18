a68a570d312c62b66883e5537150ac9a96c0e64e
levangiau <giaubale321@gmail.com>
2022-09-18 15:24:40 +0700

    Học cách xóa record và DOMContentLoaded trong JavaScript

--- src/routes/courses.js
+++ src/routes/courses.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const courseController = require('../app/controllers/CourseController');
 // chú ý tuyến đường sẽ đọc từ trên xuống nếu như mặc định ưu tiên để đầu thì nó sẽ k đọc tiếp những route sau nữa
+router.delete('/:id', courseController.delete);
 router.put('/:id', courseController.update);
 router.get('/:id/edit', courseController.edit);
 router.post('/store', courseController.store);
